451285250d815c142fb6cc6214ce2b62ba8d9e00
Gabe Lafontant <glafonta@gmail.com>
2020-10-18 12:44:08 -0400

    reminder of lifecycles while getting data to dynamically render

--- src/components/FetchUser.jsx
+++ src/components/FetchUser.jsx
@@ -10,10 +10,25 @@ export default class FetchUser extends React.Component {
     const url = "https://api.randomuser.me/";
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    this.setState({person: data.results[0], isLoading: false})
   }
 
   render() {
-    return <div>loading...</div>;
+    if (this.state.isLoading) {
+      return <div>loading...</div>;
+    }
+
+    if (!this.state.person) {
+      return <div>didn't get a person</div>;
+    }
+
+    return (
+      <div>
+        <div>{this.state.person.name.title}</div>
+        <div>{this.state.person.name.first}</div>
+        <div>{this.state.person.name.last}</div>
+        <img src={this.state.person.picture.large}/>
+      </div>
+    )
   }
 }
